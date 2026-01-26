215a317ff50dd947fe3615a30d73e139c2ab4801
RunAtTekky <varunrawat343@gmail.com>
2026-01-26 16:07:59 +0530

    implement in-memory-player-store

--- backend/pkg/storage/memory/player_store.go
+++ backend/pkg/storage/memory/player_store.go
@@ -1,14 +1,35 @@
 package memory
 
-import "github.com/runattekky/footyrate/models"
+import (
+	"fmt"
+	"sync"
+
+	"github.com/runattekky/footyrate/models"
+)
 
 type InMemoryPlayerStore struct {
+	store map[string]*models.Player
+	lock  sync.RWMutex
 }
 
 func (f *InMemoryPlayerStore) GetByID(id string) (*models.Player, error) {
-	return nil, nil
+	f.lock.RLock()
+	defer f.lock.Unlock()
+
+	player, ok := f.store[id]
+	if !ok {
+		return nil, fmt.Errorf("Player not found")
+	}
+
+	return player, nil
 }
 
 func (f *InMemoryPlayerStore) Save(player *models.Player) error {
+	f.lock.RLock()
+	defer f.lock.Unlock()
+
+	name := (*player).GetName()
+	f.store[name] = player
+
 	return nil
 }
