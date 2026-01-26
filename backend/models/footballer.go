4619dcab11c069be264eaae087b6b8c33b983a50
RunAtTekky <varunrawat343@gmail.com>
2026-01-26 13:03:55 +0530

    add: Getters and Setters on Player interface

--- backend/models/footballer.go
+++ backend/models/footballer.go
@@ -8,14 +8,46 @@ type Footballer struct {
 	Rounds   int
 }
 
+func (f *Footballer) GetName() string {
+	return f.Name
+}
+
 func (f *Footballer) GetELO() float32 {
 	return f.ELO
 }
 
-func (f *Footballer) ChangeELO(change float32) {
-	f.ELO += change
+func (f *Footballer) GetImgURL() string {
+	return f.ImgURL
 }
 
 func (f *Footballer) GetKfactor() int {
 	return f.K_Factor
+}
+
+func (f *Footballer) GetRounds() int {
+	return f.Rounds
+}
+
+func (f *Footballer) SetName(name string) {
+	f.Name = name
+}
+
+func (f *Footballer) SetELO(elo float32) {
+	f.ELO = elo
+}
+
+func (f *Footballer) SetImgURL(imgURL string) {
+	f.ImgURL = imgURL
+}
+
+func (f *Footballer) SetKfactor(k_factor int) {
+	f.K_Factor = k_factor
+}
+
+func (f *Footballer) SetRounds(rounds int) {
+	f.Rounds = rounds
+}
+
+func (f *Footballer) ChangeELO(change float32) {
+	f.ELO += change
 }
--- backend/models/player.go
+++ backend/models/player.go
@@ -1,7 +1,17 @@
 package models
 
 type Player interface {
+	GetName() string
 	GetELO() float32
+	GetImgURL() string
 	GetKfactor() int
+	GetRounds() int
+
+	SetName(string)
+	SetELO(float32)
+	SetImgURL(string)
+	SetKfactor(int)
+	SetRounds(int)
+
 	ChangeELO(change float32)
 }
