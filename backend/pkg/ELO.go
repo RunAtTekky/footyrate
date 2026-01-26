4a6e42dcc117bc4c6341c8dfa7c2f7739426acd5
RunAtTekky <varunrawat343@gmail.com>
2026-01-26 12:16:42 +0530

    round till 2 decimal places

--- backend/pkg/ELO.go
+++ backend/pkg/ELO.go
@@ -1,5 +1,15 @@
 package pkg
 
+import "math"
+
+const places float32 = 100
+
 func getELOchange(winnerELO, loserELO float32) float32 {
-	return 0.0
+	var ELOchange float32 = 0.0
+	return roundTo2DecimalPlaces(ELOchange)
+}
+
+func roundTo2DecimalPlaces(num float32) float32 {
+	rounded := math.Round(float64(num*places)) / float64(places)
+	return float32(rounded)
 }
