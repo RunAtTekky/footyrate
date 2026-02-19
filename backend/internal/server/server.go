739653bd7eb461a19ae6d4cde13db231cb5d3905
RunAtTekky <varunrawat343@gmail.com>
2026-02-19 19:35:40 +0530

    save the choice of player into database

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -18,26 +18,32 @@ type compareResponse struct {
 	PlayerTwo *models.Footballer `json:"p2"`
 }
 
+type compareRequest struct {
+	Winner string `json:"winner"`
+	Loser  string `json:"loser"`
+}
+
 func NewPlayerServer(store models.PlayerStore) *PlayerServer {
 	p := new(PlayerServer)
 	p.game = pkg.NewGame(store)
 
 	router := http.NewServeMux()
-	router.Handle("/players", http.HandlerFunc(p.playersHandle))
-	router.Handle("/compare", http.HandlerFunc(p.compareHandle))
+	router.HandleFunc("/players", p.getPlayerList)
+	router.HandleFunc("GET /compare", p.getTwoPlayers)
+	router.HandleFunc("POST /compare", p.saveVote)
 
 	p.Handler = router
 	return p
 }
 
-func (p *PlayerServer) playersHandle(w http.ResponseWriter, r *http.Request) {
+func (p *PlayerServer) getPlayerList(w http.ResponseWriter, r *http.Request) {
 	players, _ := p.game.Store.GetAllPlayers()
 	w.Header().Set("Content-Type", "application/json")
 
 	json.NewEncoder(w).Encode(players)
 }
 
-func (p *PlayerServer) compareHandle(w http.ResponseWriter, r *http.Request) {
+func (p *PlayerServer) getTwoPlayers(w http.ResponseWriter, r *http.Request) {
 	p1, p2, _ := p.game.GetTwoOpps()
 	response := compareResponse{
 		PlayerOne: p1,
@@ -46,4 +52,22 @@ func (p *PlayerServer) compareHandle(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+}
+
+func (p *PlayerServer) saveVote(w http.ResponseWriter, r *http.Request) {
+	var req compareRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	winner, _ := p.game.Store.GetByID(req.Winner)
+	loser, _ := p.game.Store.GetByID(req.Loser)
+	p.game.SaveChoice(winner, loser)
+
+	response := map[string]string{"status": "recorded"}
+	json.NewEncoder(w).Encode(response)
+
 }
